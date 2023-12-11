88506eb4787c9a0bd0aa4c677af21ff335759d5b
Ivan Surovegin <givenrous@yandex.ru>
2023-12-11 11:07:51 +0300

    решение

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -8,8 +8,13 @@ class Entity {
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
-
+  static list(data, callback) {
+    createRequest({
+      method: "GET",
+      data,
+      url: this.URL,
+      callback,
+    });
   }
 
   /**
@@ -18,14 +23,24 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    createRequest({
+      method: "PUT",
+      data,
+      url: this.URL,
+      callback,
+    });
   }
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
-
+  static remove(data, callback) {
+    createRequest({
+      method: "DELETE",
+      data,
+      url: this.URL,
+      callback,
+    });
   }
 }
